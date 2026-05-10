393afb632892f0e19140457c684bfc7ae61e9809
nic <139033898+dicnunz@users.noreply.github.com>
2026-05-10 02:53:11 -0400

    Stabilize demo e2e server lifecycle

--- tests/demo-app.e2e.spec.ts
+++ tests/demo-app.e2e.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 import { loadDemoSpecs } from "./helpers/demoSpecs";
-import { demoBaseUrl, startDemoServer, stopDemoServer, type DemoServerHandle } from "./helpers/demoServer";
+import { demoBaseUrl } from "./helpers/demoServer";
 import { runDemoSpec } from "./helpers/demoRunner";
 
 const specs = loadDemoSpecs();
@@ -9,16 +9,6 @@ const passingSpecs = specs.filter((spec) => spec.id !== "regression-catch");
 const regressionSpec = specs.find((spec) => spec.id === "regression-catch");
 
 test.describe.configure({ mode: "serial" });
-
-let server: DemoServerHandle;
-
-test.beforeAll(async () => {
-  server = await startDemoServer();
-});
-
-test.afterAll(async () => {
-  await stopDemoServer(server);
-});
 
 for (const spec of passingSpecs) {
   test(spec.id, async ({ page }) => {
